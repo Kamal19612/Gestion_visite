51713a8af9be768f9794e48bf9b7b93241136354
Kamal19612 <ouedraogokamalelise@gmail.com>
2025-11-12 10:19:21 +0000

    initialisation du projet

---
+++ src/test/java/com/NativIA/GestionVisite/GestionVisiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NativIA.GestionVisite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVisiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
